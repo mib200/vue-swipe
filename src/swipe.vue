0440ab948273a470e3bce874fde75922c3acc30f
Manish Kumar <mib200@gmail.com>
2016-12-01 00:10:41 +0530

    removing dependency on win-dom. SSR now works

--- src/swipe.vue
+++ src/swipe.vue
@@ -57,8 +57,102 @@
 </template>
 
 <script>
-  import { once } from 'wind-dom/src/event';
-  import { addClass, removeClass } from 'wind-dom/src/class';
+	const inBrowser = typeof window !== 'undefined';
+
+	var bindOnce = function(el, event, fn) {
+		if(inBrowser) {
+			var listener = function() {
+				if (fn) {
+					fn.apply(this, arguments);
+				}
+				// unbindEvent(el, event, listener);
+			};
+			// bindEvent(el, event, listener);
+		};
+	};
+
+	var addClass = function (el, cls) {
+		if (!el) return;
+		var curClass = el.className;
+		var classes = (cls || '').split(' ');
+
+		for (var i = 0, j = classes.length; i < j; i++) {
+			var clsName = classes[i];
+			if (!clsName) continue;
+
+			if (el.classList) {
+				el.classList.add(clsName);
+			} else {
+				if (!hasClass(el, clsName)) {
+					curClass += ' ' + clsName;
+				}
+			}
+		}
+		if (!el.classList) {
+			el.className = curClass;
+		}
+	};
+
+	var bindEvent = (function() {
+		if(inBrowser) {
+			if(document.addEventListener) {
+				return function(element, event, handler) {
+					if (element && event && handler) {
+						element.addEventListener(event, handler, false);
+					}
+				};
+			} else {
+				return function(element, event, handler) {
+					if (element && event && handler) {
+						element.attachEvent('on' + event, handler);
+					}
+				};
+			}
+		}
+	})();
+
+	var unbindEvent = (function() {
+		if(inBrowser) {
+			if(document.removeEventListener) {
+				return function(element, event, handler) {
+					if (element && event) {
+						element.removeEventListener(event, handler, false);
+					}
+				};
+			} else {
+				return function(element, event, handler) {
+					if (element && event) {
+						element.detachEvent('on' + event, handler);
+					}
+				};
+			}
+		}
+	})();
+
+	var removeClass = function (el, cls) {
+		if (!el || !cls) return;
+		var classes = cls.split(' ');
+		var curClass = ' ' + el.className + ' ';
+
+		for (var i = 0, j = classes.length; i < j; i++) {
+			var clsName = classes[i];
+			if (!clsName) continue;
+
+			if (el.classList) {
+				el.classList.remove(clsName);
+			} else {
+				if (hasClass(el, clsName)) {
+					curClass = curClass.replace(' ' + clsName + ' ', ' ');
+				}
+			}
+		}
+		if (!el.classList) {
+			el.className = trim(curClass);
+		}
+	};
+
+  // import { once } from 'wind-dom/src/event';
+  // import { addClass, removeClass } from 'wind-dom/src/class';
 
   export default {
     name: 'mt-swipe',
@@ -153,7 +247,7 @@
             }
           };
 
-          once(element, 'webkitTransitionEnd', transitionEndCallback);
+          bindOnce(element, 'webkitTransitionEnd', transitionEndCallback);
           setTimeout(transitionEndCallback, speed + 100); // webkitTransitionEnd maybe not fire on lower version android.
         } else {
           element.style.webkitTransition = '';
